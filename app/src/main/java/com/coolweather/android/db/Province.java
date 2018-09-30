d67027d7deca156a217a2732a8cbe580ddcdc557
Tony <1347231479@qq.com>
2018-09-30 21:21:51 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -0,0 +1,29 @@
+package com.coolweather.android.db;
+
+/**
+ * Created by 13472 on 2018/9/30.
+ */
+
+public class Province extends DataSupport{  //依赖包
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public  String getProvinceName(){
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceName=provinceName;
+    }
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode = provinceCode;
+    }
+}
